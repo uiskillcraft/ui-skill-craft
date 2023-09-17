445035f32b3296b9afc823868ce0869445078346
MoezKouni <mo.elkouni@gmail.com>
2023-09-18 01:36:38 +0200

    show challenges

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-// import Challenges from "@/components/shared/Challenges";
-// import Menu from "@/components/shared/Menu";
-import { Button } from "@/components/ui/button";
+import Challenges from "@/components/shared/Challenges";
 
 export default function Home() {
   return (
     <main>
-      {/* <Menu /> */}
-      <Button>Click</Button>
-      {/* <div className="container mt-4">
+      <div className="container mt-4">
         <Challenges />
-      </div> */}
+      </div>
     </main>
   );
 }
